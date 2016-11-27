b9c928eb674ff50bade7f93c7335b1c467771cba
bwnyasse <risbonyasse@gmail.com>
2016-11-27 19:20:30 +0100

    Correct files copy from host to target server

--- init/curlsh/init.sh
+++ init/curlsh/init.sh
@@ -15,25 +15,36 @@ readonly MAGENTA="\\033[1;35m"
 readonly GRAS="\033[1m"
 readonly END="\\033[1;00m"
 
+ACTUAL_INIT_REPO=vagrant@192.168.33.10:/vagrant/bwbw/init
+BW_INIT_DIRECTORY=.bw-init
+
+# _ __  __ _(_)_ _
+#| '  \/ _` | | ' \
+#|_|_|_\__,_|_|_||_|
+
 info() {
 	echo -e "$GREEN [INFO] $* $END"
 }
 
+if [[ -d $BW_INIT_DIRECTORY ]]; then
+    warn " Find a previous installation directory, it will be deleted !"
+    rm -rf $BW_INIT_DIRECTORY
+fi
+
 info " Attemp to download necessary files to setup BW environment ..."
 echo " "
-
 #FIXME : Must change , scp right now for project understanding
+scp -rpq -o LogLevel=QUIET $ACTUAL_INIT_REPO $BW_INIT_DIRECTORY
+#TODO Handle error !
 
+find $BW_INIT_DIRECTORY/ -type f -name "*.sh" -exec chmod +x {} \;
 
 cat <<-EOF
     Welcome to
-
-    ___.          ___.
-    \_ |____  _  _\_ |____  _  __
-     | __ \ \/ \/ /| __ \ \/ \/ /
-     | \_\ \     / | \_\ \     /
-     |___  /\/\_/  |___  /\/\_/
-         \/            \/
+     _           _
+    | |____ __ _| |____ __ __
+    | '_ \ V  V / '_ \ V  V /
+    |_.__/\_/\_/|_.__/\_/\_/
 
      ---- (c)$(date +%Y) bwnyasse
 
